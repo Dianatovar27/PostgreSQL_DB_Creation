58a332af3e3db3eee9310da7744d2d7e88e3085b
Diana Tovar Reolon <dianatovarreolon@gmail.com>
2024-08-04 19:50:57 -0500

    updated database date default, and student table birthday to Date datatype, lowerscased all table and column names

--- postgresql_db.sql
+++ postgresql_db.sql
@@ -1,21 +1,22 @@
-CREATE TABLE Students (Student_ID SERIAL PRIMARY KEY, Full_Name TEXT, Age INTEGER, Address TEXT);
-INSERT INTO Students (Full_Name, Age, Address) VALUES ('XXXXX' ,0,' ');
+SET datestyle = 'DMY';
+CREATE TABLE students (student_id SERIAL PRIMARY KEY, full_name TEXT, birthday DATE, address TEXT);
+/*Table content is going to be added through py in the future*/
 
-CREATE TABLE Coaches ( Coach_ID SERIAL PRIMARY KEY, Full_Name TEXT);
-INSERT INTO Coaches (Full_Name) VALUES ('XXXXX');
+CREATE TABLE coaches (coach_id SERIAL PRIMARY KEY, full_name TEXT);
+INSERT INTO coaches (full_name) VALUES ('XXXXX');
 
-CREATE TABLE Laptops ( Laptop_ID SERIAL PRIMARY KEY, NLaptop INTEGER);
-INSERT INTO Laptops (NLaptop) VALUES (1);
+CREATE TABLE laptops (laptop_id SERIAL PRIMARY KEY, laptop_number INTEGER);
+INSERT INTO laptops (laptop_number) VALUES (1);
 
-CREATE TYPE weekday AS ENUM ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+CREATE TYPE WeekdayEnum AS ENUM ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 
-CREATE TABLE Classes(Class_ID SERIAL PRIMARY KEY, Class_Name TEXT, Class_Type VARCHAR(100), Coach_ID INTEGER, Weekday weekday, Start_Time TIME, Duration_Minutes INTEGER );
+CREATE TABLE classes(class_id SERIAL PRIMARY KEY, class_name TEXT, class_type VARCHAR(100), coach_id INTEGER, weekday WeekdayEnum, start_time TIME, duration_minutes INTEGER);
 
-ALTER TABLE Classes
-ADD CONSTRAINT fk_Coaches
-FOREIGN KEY (Coach_ID)
-REFERENCES Coaches (Coach_ID)
+ALTER TABLE classes
+ADD CONSTRAINT fk_coaches
+FOREIGN KEY (coach_id)
+REFERENCES coaches (coach_id)
 ON UPDATE CASCADE;
 
-INSERT INTO Classes (Class_Name, Class_Type, Coach_ID, Weekday, Start_Time, Duration_Minutes)
+INSERT INTO classes (class_name, class_type, coach_id, weekday, start_time, duration_minutes)
 VALUES ('XXXXXX', 'Class', 9, 'Tuesday', '16:00:00', 50);
